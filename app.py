1dfcbff8baa3b3c29906c097cc1d16a10ac69a89
supansa108 <51729293+supansa108@users.noreply.github.com>
2019-08-07 09:20:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,11 +24,11 @@ def callback():
     userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
     if (userText == 'สว้สดี') :
-    sendText(user,'ดีจร้า')
+          sendText(user,'ดีจร้า')
     elif (userText == 'ทำไรอยู่') :
-    sendText(user,'กินข้าว')
+          sendText(user,'กินข้าว')
     else :
-    sendText(user,== 'ไม่เข่าใจ'):
+          sendText(user,== 'ไม่เข่าใจ'):
 
     return '',200
 
